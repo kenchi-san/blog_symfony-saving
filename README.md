f6cb636e1aeafa687b0e32e98ff8ba0a093c5697
kenchi-san <charon.hugo@gmail.com>
2018-07-24 04:33:13 +0200

    partie ajout de commentaire down

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# blog_symfony
+Run your application:
+    1. Change to the project directory
+    2. Create your code repository with the git init command
+    3. Execute the php -S 127.0.0.1:8000 -t public command
+    4. Browse to the http://localhost:8000/ URL.
+
+       Quit the server with CTRL-C.
+       Run composer require server --dev for a better web server.
+
+  * Read the documentation at https://symfony.com/doc
+
+
+ Database Configuration
+
+
+  * Modify your DATABASE_URL config in .env
+
+  * Configure the driver (mysql) and
+    server_version (5.7) in config/packages/doctrine.yaml
+
+
+ How to test?
+
+
+  * Write test cases in the tests/ folder
+  * Run php bin/phpunit
+
+
+
+etape et commandes git:
+-> composer require server --dev (librairie pour dev)
+-> php bin/console server:run (demarage serveur)
+-> php bin/console make:controller (creer un controller)
+-> php bin/console make:entity (creer l'entity)
+-> php bin/console doctrine:database:create
+-> php bin/console make:migration (check entre les dossier et la bdd et fait une maj)
+-> php bin/console doctrine:migration:migrate (met les informations sur le serveur)
+-> php bin/console make:form
+
+
+settup fixture: fausse donner pour la bdd
+-> composer require orm-fixtures --dev (import la librairie pour faire)
+-> php bin/console make:fixtures (creer le dossier fixture)
+-> php bin/console doctrine:fixtures:load (envoi les fausses infos dans la bdd)
+
+-> creer facilement un faker-> aller sur php faker (le premier)->settup => composer require fzaninotto/faker --dev
---
+++ src/Controller/BlogController.php
@@ -0,0 +1,105 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Doctrine\Common\DataFixtures\ReferenceRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\NotNull;
+use App\Form\ArticleType;
+use App\Entity\Comment;
+use App\Form\CommentType;
+use Doctrine\Common\Persistence\PersistentObject;
+
+class BlogController extends Controller
+{
+
+    /**
+     *
+     * @Route("/blog", name="blog")
+     */
+    public function index(ArticleRepository $repo)
+    {
+        $Articles = $repo->findAll();
+        
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+            'articles' => $Articles
+        ]);
+    }
+
+    /**
+     *
+     * @route("/", name="home")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function home()
+    {
+        return $this->render('blog/home.html.twig');
+    }
+
+    /**
+     *
+     * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit", name="blog-edit")
+     */
+    public function form(Article $article = null, Request $request, ObjectManager $manager)
+    {
+        if (! $article) {
+            $article = new Article();
+        }
+        
+        $form = $this->createForm(ArticleType::class, $article);
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (! $article->getId()) {
+                $article->setCreatedAt(new \DateTime());
+            }
+            // dump($article);
+            $manager->persist($article);
+            $manager->flush();
+            return $this->redirectToRoute('blog_show', [
+                'id' => $article->getId()
+            ]);
+        }
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView(),
+            'editMode' => $article->getId() !== null
+        ]);
+    }
+
+    /**
+     *
+     * @Route("/blog/{id}", name="blog_show")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function show(Article $article, Request $request, ObjectManager $manager)
+    {
+        $comment = new Comment();
+        $form =$this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        
+        
+        
+        if ($form->isSubmitted() && $form->isValid()){
+            $comment->setCreatedAt(new \DateTime())
+            ->setArticle($article);
+            
+            $manager->persist($comment);
+            $manager->flush();
+            
+            return $this->redirectToRoute('blog_show',['id'=>$article->getId() ] );
+        }
+        
+        return $this->render('blog/show.html.twig', [
+            'article' => $article,
+            'commentForm'=>$form->createView()
+        
+        ]);
+    }
+}
